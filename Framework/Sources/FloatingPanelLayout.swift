8af248592f5818287f0217d3579cf3155811c695
Yon Montoto <yon.montoto@gmail.com>
2018-10-23 12:50:06 -0400

    Adjusted the width of the Landscape mode of panel

--- Framework/Sources/FloatingPanelLayout.swift
+++ Framework/Sources/FloatingPanelLayout.swift
@@ -79,8 +79,8 @@ public class FloatingPanelDefaultLandscapeLayout: FloatingPanelLayout {
 
     public func prepareLayout(surfaceView: UIView, in view: UIView) -> [NSLayoutConstraint] {
         return [
-            surfaceView.leftAnchor.constraint(equalTo: view.sideLayoutGuide.leftAnchor, constant: 8.0),
-            surfaceView.widthAnchor.constraint(equalToConstant: 291),
+            surfaceView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            surfaceView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
             ]
     }
 }
