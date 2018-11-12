aa6cd3421e0dc879bbe84bd4c0b954d7b2d7a89a
Shin Yamamoto <shin@scenee.com>
2018-11-12 11:09:37 +0900

    Add comments and fix typos

--- Framework/Sources/FloatingPanelLayout.swift
+++ Framework/Sources/FloatingPanelLayout.swift
@@ -183,7 +183,7 @@ class FloatingPanelLayoutAdapter {
 
         // Fixed constraints of surface and backdrop views
         let surfaceConstraints = layout.prepareLayout(surfaceView: surfaceView, in: parent.view!)
-        let backdroptConstraints = [
+        let backdropConstraints = [
             backdropView.topAnchor.constraint(equalTo: parent.view.topAnchor,
                                               constant: 0.0),
             backdropView.leftAnchor.constraint(equalTo: parent.view.leftAnchor,
@@ -193,7 +193,7 @@ class FloatingPanelLayoutAdapter {
             backdropView.bottomAnchor.constraint(equalTo: parent.view.bottomAnchor,
                                                  constant: 0.0),
             ]
-        fixedConstraints = surfaceConstraints + backdroptConstraints
+        fixedConstraints = surfaceConstraints + backdropConstraints
 
         // Flexible surface constarints for full, half, tip and off
         fullConstraints = [
@@ -223,6 +223,10 @@ class FloatingPanelLayoutAdapter {
         }
 
         NSLayoutConstraint.deactivate(heightConstraints)
+        // Must use the parent height, not the screen height because safe area insets
+        // of the parent are relative values. For example, a view controller in
+        // Navigation controller's safe area insets and frame can be changed whether
+        // the navigation bar is translucent or not.
         let height = self.parent.view.bounds.height - (safeAreaInsets.top + fullInset)
         heightConstraints = [
             surfaceView.heightAnchor.constraint(equalToConstant: height)
