f9a5386dc71cb47f8b733831fee1e12925799e72
Shin Yamamoto <shin@scenee.com>
2018-12-07 14:34:39 +0900

    Add SamplesObjC app
    * Add build job in .travis.yml

---
+++ Examples/SamplesObjC/SamplesObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SamplesObjC
+//
+//  Created by Shin Yamamoto on 2018/12/07.
+//  Copyright © 2018 Shin Yamamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
