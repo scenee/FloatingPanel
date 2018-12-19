84648574f40c029f4d6ce6469e9181c2cba9a5a9
Shin Yamamoto <shin@scenee.com>
2018-12-19 15:15:15 +0900

    Improve Sample apps for debugging
    - Add InspectableViewController
    - Add version label in Samples app

--- Examples/Samples/Sources/UIExtensions.swift
+++ Examples/Samples/Sources/UIExtensions.swift
@@ -5,21 +5,39 @@
 
 import UIKit
 
-extension UIView {
-    var layoutInsets: UIEdgeInsets {
-        if #available(iOS 11.0, *) {
-            return safeAreaInsets
-        } else {
-            return layoutMargins
-        }
-    }
+protocol LayoutGuideProvider {
+    var topAnchor: NSLayoutYAxisAnchor { get }
+    var bottomAnchor: NSLayoutYAxisAnchor { get }
+}
+extension UILayoutGuide: LayoutGuideProvider {}
 
-    var layoutGuide: UILayoutGuide {
-        if #available(iOS 11.0, *) {
-            return safeAreaLayoutGuide
-        } else {
-            return layoutMarginsGuide
-        }
+class CustomLayoutGuide: LayoutGuideProvider {
+    let topAnchor: NSLayoutYAxisAnchor
+    let bottomAnchor: NSLayoutYAxisAnchor
+    init(topAnchor: NSLayoutYAxisAnchor, bottomAnchor: NSLayoutYAxisAnchor) {
+        self.topAnchor = topAnchor
+        self.bottomAnchor = bottomAnchor
     }
 }
 
+extension UIViewController {
+    var layoutInsets: UIEdgeInsets {
+        if #available(iOS 11.0, *) {
+            return view.safeAreaInsets
+        } else {
+            return UIEdgeInsets(top: topLayoutGuide.length,
+                                left: 0.0,
+                                bottom: bottomLayoutGuide.length,
+                                right: 0.0)
+        }
+    }
+
+    var layoutGuide: LayoutGuideProvider {
+        if #available(iOS 11.0, *) {
+            return view!.safeAreaLayoutGuide
+        } else {
+            return CustomLayoutGuide(topAnchor: topLayoutGuide.bottomAnchor,
+                                     bottomAnchor: bottomLayoutGuide.topAnchor)
+        }
+    }
+}
