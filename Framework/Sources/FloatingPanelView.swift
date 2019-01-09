52fa52cc2d6793a24f809dc8a5077989ca6343e1
Shin Yamamoto <shin@scenee.com>
2019-01-09 09:32:15 +0900

    Fix the event handling on presentation modally
    If an alpha of the controller's backdrop view is zero, the presentation controller
    must not block any touch event outside of surface view.

--- Framework/Sources/FloatingPanelView.swift
+++ Framework/Sources/FloatingPanelView.swift
@@ -6,13 +6,14 @@
 import UIKit
 
 class FloatingPanelPassThroughView: UIView {
+    public weak var eventForwardingView: UIView?
     public override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
-        let view = super.hitTest(point, with: event)
-        switch view {
-        case is FloatingPanelPassThroughView:
-            return nil
+        let hitView = super.hitTest(point, with: event)
+        switch hitView {
+        case self:
+            return eventForwardingView?.hitTest(self.convert(point, to: eventForwardingView), with: event)
         default:
-            return view
+            return hitView
         }
     }
 }
