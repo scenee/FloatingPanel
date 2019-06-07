3cbb092a2ccecf70ffbd518340da9caac82d84de
Robbie Trencheny <me@robbiet.us>
2019-06-06 22:17:56 -0700

    Set available versions

--- Package.swift
+++ Package.swift
@@ -19,5 +19,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "FloatingPanel", path: "Framework/Sources"),
-    ]
+    ],
+    swiftLanguageVersions: [.version("5")]
 )
