635fd3619d4bf08eb8bb1630cd6003cb54a859de
Robbie Trencheny <me@robbiet.us>
2019-06-06 22:26:37 -0700

    Fix syntax

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "FloatingPanel",
+    platforms: [
+        .iOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -19,9 +22,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "FloatingPanel", path: "Framework/Sources"),
-    ],
-    platforms: [
-        .iOS(.v10)
     ],
     swiftLanguageVersions: [.version("5")]
 )
