fd4e6fea618366b95b7c645b0448cb3d7ee3f097
Shin Yamamoto <shin@scenee.com>
2020-05-16 14:52:02 +0900

    Add UISpringTimingParameters(decelerationRate:frequencyResponse:initialVelocity:)

--- Framework/Sources/UIExtensions.swift
+++ Framework/Sources/UIExtensions.swift
@@ -142,6 +142,11 @@ extension UIScrollView {
 }
 
 extension UISpringTimingParameters {
+    public convenience init(decelerationRate: CGFloat, frequencyResponse: CGFloat, initialVelocity: CGVector = .zero) {
+        let dampingRatio = CoreGraphics.log(decelerationRate) / (-4 * .pi * 0.001)
+        self.init(dampingRatio: dampingRatio, frequencyResponse: frequencyResponse, initialVelocity: initialVelocity)
+    }
+
     public convenience init(dampingRatio: CGFloat, frequencyResponse: CGFloat, initialVelocity: CGVector = .zero) {
         let mass = 1 as CGFloat
         let stiffness = pow(2 * .pi / frequencyResponse, 2) * mass
