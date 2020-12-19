bec37ed6ccb9e75c7ac370ab14e81405cf8b18f7
Federico Zanetello <zntfdr@gmail.com>
2020-12-19 11:08:35 +0700

    add cornerCurve option to SurfaceAppearance

--- Sources/SurfaceView.swift
+++ Sources/SurfaceView.swift
@@ -50,6 +50,12 @@ public class SurfaceAppearance: NSObject {
     /// On iOS 10, they are not automatically masked because of a UIVisualEffectView issue. See https://forums.developer.apple.com/thread/50854
     public var cornerRadius: CGFloat = 0.0
 
+    /// Defines the curve used for rendering the rounded corners of the layer.
+    ///
+    /// Defaults to `.circular`.
+    @available(iOS 13.0, *)
+    public lazy var cornerCurve: CALayerCornerCurve = .circular
+
     /// An array of shadows used to create drop shadows underneath a surface view.
     public var shadows: [Shadow] = [Shadow()]
 
@@ -395,6 +401,10 @@ public class SurfaceView: UIView {
         } else {
             // Can't use `containerView.layer.mask` because of a UIVisualEffectView issue in iOS 10, https://forums.developer.apple.com/thread/50854
             // Instead, a user should display rounding corners appropriately.
+        }
+
+        if #available(iOS 13, *) {
+            containerView.layer.cornerCurve = appearance.cornerCurve
         }
     }
 
