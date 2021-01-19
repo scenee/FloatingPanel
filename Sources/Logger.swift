34809bd8eaa81a3507540793e10ce8a77fe7dee7
Shin Yamamoto <shin@scenee.com>
2021-01-19 12:06:23 +0900

    Show debug logs in console.app

--- Sources/Logger.swift
+++ Sources/Logger.swift
@@ -68,7 +68,7 @@ struct Logger {
 
         hook?(log, level)
 
-        os_log("%@", log: osLog, type: level.osLogType, log)
+        os_log("%{public}@", log: osLog, type: level.osLogType, log)
     }
 
     private func getPrettyFunction(_ function: String, _ file: String) -> String {
