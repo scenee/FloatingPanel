8a28e9e5aa71be680267c8d87504ce3206aa66fd
Federico Zanetello <zntfdr@gmail.com>
2021-07-04 17:30:33 +0700

    improve indentation

--- Examples/Maps-SwiftUI/Maps/View+floatingPanel.swift
+++ Examples/Maps-SwiftUI/Maps/View+floatingPanel.swift
@@ -6,8 +6,11 @@ extension View {
     func floatingPanel<FloatingPanelContent: View>(
         @ViewBuilder _ floatingPanelContent: @escaping (_: FloatingPanelProxy) -> FloatingPanelContent
     ) -> some View {
-        FloatingPanelView(content: { self }, floatingPanelContent: floatingPanelContent)
-            .ignoresSafeArea()
+        FloatingPanelView(
+            content: { self },
+            floatingPanelContent: floatingPanelContent
+        )
+        .ignoresSafeArea()
     }
 }
 
