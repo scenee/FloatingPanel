6148768de225cd63a00d5cdfd70eee680845f449
Federico Zanetello <zntfdr@gmail.com>
2021-07-05 21:18:42 +0700

    add documentation and credits

--- Examples/Maps-SwiftUI/Maps/HostingCell.swift
+++ Examples/Maps-SwiftUI/Maps/HostingCell.swift
@@ -2,92 +2,93 @@
 
 import SwiftUI
 
+/// A `UITableViewCell` that accepts a SwiftUI view as its content.
+///
+/// Credits to https://noahgilmore.com/blog/swiftui-self-sizing-cells/ .
 public final class HostingCell<Content: View>: UITableViewCell {
-  private let hostingController = UIHostingController<Content?>(
-    rootView: nil,
-    ignoresKeyboard: true
-  )
+    private let hostingController = UIHostingController<Content?>(
+        rootView: nil,
+        ignoresKeyboard: true
+    )
 
-  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-    super.init(style: style, reuseIdentifier: reuseIdentifier)
-    hostingController.view.backgroundColor = nil
-    backgroundColor = .clear
-  }
-
-  @available(*, unavailable)
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  public func set(rootView: Content, parentController: UIViewController) {
-    hostingController.rootView = rootView
-    hostingController.view.invalidateIntrinsicContentSize()
-
-    let requiresControllerMove = hostingController.parent != parentController
-    if requiresControllerMove {
-      parentController.addChild(hostingController)
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        hostingController.view.backgroundColor = nil
+        backgroundColor = .clear
     }
 
-    if !contentView.subviews.contains(hostingController.view) {
-      contentView.addSubview(hostingController.view)
-      contentView.addConstraints(
-        hostingController.view.constraintsEqualTo(view: contentView)
-      )
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 
-    if requiresControllerMove {
-      hostingController.didMove(toParent: parentController)
+    public func set(rootView: Content, parentController: UIViewController) {
+        hostingController.rootView = rootView
+        hostingController.view.invalidateIntrinsicContentSize()
+
+        let requiresControllerMove = hostingController.parent != parentController
+        if requiresControllerMove {
+            parentController.addChild(hostingController)
+        }
+
+        if !contentView.subviews.contains(hostingController.view) {
+            contentView.addSubview(hostingController.view)
+            hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addConstraints([
+                hostingController.view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+                hostingController.view.topAnchor.constraint(equalTo: contentView.topAnchor),
+                hostingController.view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+                hostingController.view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            ])
+        }
+
+        if requiresControllerMove {
+            hostingController.didMove(toParent: parentController)
+        }
     }
-  }
 }
 
-public extension UIView {
-  func constraintsEqualTo(view: UIView) -> [NSLayoutConstraint] {
-    translatesAutoresizingMaskIntoConstraints = false
-    return [
-      leadingAnchor.constraint(equalTo: view.leadingAnchor),
-      topAnchor.constraint(equalTo: view.topAnchor),
-      trailingAnchor.constraint(equalTo: view.trailingAnchor),
-      bottomAnchor.constraint(equalTo: view.bottomAnchor)
-    ]
-  }
-}
-
+/// This extension fixes a safe area issue happening with `HostingCell` when a
+/// keyboard is shown.
+///
+/// The bug is present only in iOS 14.0-14.2 and only if a keyboard is shown.
+///
+/// Credits to https://steipete.me/posts/disabling-keyboard-avoidance-in-swiftui-uihostingcontroller/
 @available(iOS, introduced: 13, deprecated: 14.2, message: "No longer necessary after iOS 14.2.")
 extension UIHostingController {
-  public convenience init(rootView: Content, ignoresKeyboard: Bool) {
-    self.init(rootView: rootView)
+    public convenience init(rootView: Content, ignoresKeyboard: Bool) {
+        self.init(rootView: rootView)
 
-    if ignoresKeyboard {
-      guard let viewClass = object_getClass(view) else { return }
+        if ignoresKeyboard {
+            guard let viewClass = object_getClass(view) else { return }
 
-      let viewSubclassName = String(
-        cString: class_getName(viewClass)
-      ).appending("_IgnoresKeyboard")
+            let viewSubclassName = String(
+                cString: class_getName(viewClass)
+            ).appending("_IgnoresKeyboard")
 
-      if let viewSubclass = NSClassFromString(viewSubclassName) {
-        object_setClass(view, viewSubclass)
-      } else {
-        guard
-          let viewClassNameUtf8 = (viewSubclassName as NSString).utf8String,
-          let viewSubclass = objc_allocateClassPair(viewClass, viewClassNameUtf8, 0)
-        else { return }
+            if let viewSubclass = NSClassFromString(viewSubclassName) {
+                object_setClass(view, viewSubclass)
+            } else {
+                guard
+                    let viewClassNameUtf8 = (viewSubclassName as NSString).utf8String,
+                    let viewSubclass = objc_allocateClassPair(viewClass, viewClassNameUtf8, 0)
+                else { return }
 
-        if let method = class_getInstanceMethod(
-            viewClass,
-            NSSelectorFromString("keyboardWillShowWithNotification:")
-        ) {
-          let keyboardWillShow: @convention(block) (AnyObject, AnyObject) -> Void = { _, _ in }
-          class_addMethod(
-            viewSubclass,
-            NSSelectorFromString("keyboardWillShowWithNotification:"),
-            imp_implementationWithBlock(keyboardWillShow),
-            method_getTypeEncoding(method)
-          )
+                if let method = class_getInstanceMethod(
+                    viewClass,
+                    NSSelectorFromString("keyboardWillShowWithNotification:")
+                ) {
+                    let keyboardWillShow: @convention(block) (AnyObject, AnyObject) -> Void = { _, _ in }
+                    class_addMethod(
+                        viewSubclass,
+                        NSSelectorFromString("keyboardWillShowWithNotification:"),
+                        imp_implementationWithBlock(keyboardWillShow),
+                        method_getTypeEncoding(method)
+                    )
+                }
+                objc_registerClassPair(viewSubclass)
+                object_setClass(view, viewSubclass)
+            }
         }
-        objc_registerClassPair(viewSubclass)
-        object_setClass(view, viewSubclass)
-      }
     }
-  }
 }
