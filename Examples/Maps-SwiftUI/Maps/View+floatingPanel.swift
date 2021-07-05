be6971e2694a819f3381eeffa9695b19f6037e25
Federico Zanetello <zntfdr@gmail.com>
2021-07-05 17:52:11 +0700

    add floatingPanel modifier documentation

--- Examples/Maps-SwiftUI/Maps/View+floatingPanel.swift
+++ Examples/Maps-SwiftUI/Maps/View+floatingPanel.swift
@@ -3,6 +3,11 @@
 import SwiftUI
 
 extension View {
+    /// Presents a floating panel using the given closure as its content.
+    ///
+    /// - Parameters:
+    ///   - floatingPanelContent: A closure returning the content of the
+    ///     floating panel.
     public func floatingPanel<FloatingPanelContent: View>(
         @ViewBuilder _ floatingPanelContent: @escaping (_: FloatingPanelProxy) -> FloatingPanelContent
     ) -> some View {
