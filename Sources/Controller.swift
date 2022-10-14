scenee/FloatingPanel

commit 9c45c311901b90be90653aff63871ea751fae171

Fix a regression for the backdrop behavior (#466,#471)
The backdrop alpha of a panel must be updated only in `Controller.show(animated:completion:)`.
Because that prevents a backdrop flicking just before presenting a panel.

Resolve #466.

Sources/Controller.swift
@@ -398,20 +398,8 @@ open class FloatingPanelController: UIViewController {
     }
 
     private func activateLayout(forceLayout: Bool = false) {
-        floatingPanel.layoutAdapter.prepareLayout()
-
-        // preserve the current content offset if contentInsetAdjustmentBehavior is `.always`
-        var contentOffset: CGPoint?
-        if contentInsetAdjustmentBehavior == .always {
-            contentOffset = trackingScrollView?.contentOffset
-        }
-
-        floatingPanel.layoutAdapter.updateStaticConstraint()
-        floatingPanel.layoutAdapter.activateLayout(for: floatingPanel.state, forceLayout: forceLayout)
-
-        if let contentOffset = contentOffset {
-            trackingScrollView?.contentOffset = contentOffset
-        }
+        floatingPanel.activateLayout(forceLayout: forceLayout,
+                                     contentInsetAdjustmentBehavior: contentInsetAdjustmentBehavior)
     }
 
     func remove() {

Sources/Core.swift
@@ -186,6 +186,30 @@ class Core: NSObject, UIGestureRecognizerDelegate {
 
     // MARK: - Layout update
 
+    func activateLayout(forceLayout: Bool = false,
+                        contentInsetAdjustmentBehavior: FloatingPanelController.ContentInsetAdjustmentBehavior) {
+        layoutAdapter.prepareLayout()
+
+        // preserve the current content offset if contentInsetAdjustmentBehavior is `.always`
+        var contentOffset: CGPoint?
+        if contentInsetAdjustmentBehavior == .always {
+            contentOffset = scrollView?.contentOffset
+        }
+
+        layoutAdapter.updateStaticConstraint()
+        layoutAdapter.activateLayout(for: state, forceLayout: true)
+
+        // Update the backdrop alpha only when called in `Controller.show(animated:completion:)`
+        // Because that prevents a backdrop flicking just before presenting a panel(#466).
+        if forceLayout {
+            backdropView.alpha = getBackdropAlpha(for: state)
+        }
+
+        if let contentOffset = contentOffset {
+            scrollView?.contentOffset = contentOffset
+        }
+    }
+
     private func updateLayout(to target: FloatingPanelState) {
         self.layoutAdapter.activateLayout(for: target, forceLayout: true)
         self.backdropView.alpha = self.getBackdropAlpha(for: target)

commit acdef1e5c7b9e2738c50ceea94f99ba406f1375f

Change the backdrop alpha in screen orientation or size changes.
This fixes #572. The main change is that `true` is passed as a
`forceLayout` parameter into `viewWillTransition(to:with:)` callbacks.

Because it's necessary for the backdrop alpha's update when the view
size or its size classes changes.

This also fixes a regression by 9c45c31.

```diff
-        layoutAdapter.activateLayout(for: state, forceLayout: true)
+        layoutAdapter.activateLayout(for: state, forceLayout: forceLayout)
```

The behavior before the above second change indicates that the method is
working well even when `forceLayout` is set to `true` in their callbacks.

Sources/Controller.swift
@@ -316,7 +316,7 @@ open class FloatingPanelController: UIViewController {
         // Change a layout for the new view size
         if let newLayout = self.delegate?.floatingPanel?(self, layoutFor: size) {
             layout = newLayout
-            activateLayout(forceLayout: false)
+            activateLayout(forceLayout: true)
         }
 
         if view.translatesAutoresizingMaskIntoConstraints {
@@ -335,7 +335,7 @@ open class FloatingPanelController: UIViewController {
         // Change a layout for the new trait collection
         if let newLayout = self.delegate?.floatingPanel?(self, layoutFor: newCollection) {
             self.layout = newLayout
-            activateLayout(forceLayout: false)
+            activateLayout(forceLayout: true)
         }
     }
 

Sources/Core.swift
@@ -210,7 +210,7 @@ class Core: NSObject, UIGestureRecognizerDelegate {
         }
 
         layoutAdapter.updateStaticConstraint()
-        layoutAdapter.activateLayout(for: state, forceLayout: true)
+        layoutAdapter.activateLayout(for: state, forceLayout: forceLayout)
 
         // Update the backdrop alpha only when called in `Controller.show(animated:completion:)`
         // Because that prevents a backdrop flicking just before presenting a panel(#466).