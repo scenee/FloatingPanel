0e833aee3c1fd785219c948e10898c39b83c3e0b
Shin Yamamoto <shin@scenee.com>
2023-02-17 09:37:30 +0900

    Fix SettingsViewController in Samples app

--- Examples/Samples/Sources/ContentViewControllers/SettingsViewController.swift
+++ Examples/Samples/Sources/ContentViewControllers/SettingsViewController.swift
@@ -4,8 +4,8 @@ import UIKit
 import FloatingPanel
 
 final class SettingsViewController: InspectableViewController {
-    @IBOutlet weak var largeTitlesSwicth: UISwitch!
-    @IBOutlet weak var translucentSwicth: UISwitch!
+    @IBOutlet weak var largeTitlesSwitch: UISwitch!
+    @IBOutlet weak var translucentSwitch: UISwitch!
     @IBOutlet weak var versionLabel: UILabel!
 
     override func viewDidLoad() {
@@ -16,12 +16,12 @@ final class SettingsViewController: InspectableViewController {
         super.viewDidLayoutSubviews()
         if #available(iOS 11.0, *) {
             let prefersLargeTitles = navigationController!.navigationBar.prefersLargeTitles
-            largeTitlesSwicth.setOn(prefersLargeTitles, animated: false)
+            largeTitlesSwitch.setOn(prefersLargeTitles, animated: false)
         } else {
-            largeTitlesSwicth.isEnabled = false
+            largeTitlesSwitch.isEnabled = false
         }
         let isTranslucent = navigationController!.navigationBar.isTranslucent
-        translucentSwicth.setOn(isTranslucent, animated: false)
+        translucentSwitch.setOn(isTranslucent, animated: false)
     }
 
     @IBAction func toggleLargeTitle(_ sender: UISwitch) {
@@ -30,7 +30,19 @@ final class SettingsViewController: InspectableViewController {
         }
     }
     @IBAction func toggleTranslucent(_ sender: UISwitch) {
-        navigationController?.navigationBar.isTranslucent = sender.isOn
+        // White non-translucent navigation bar, supports dark appearance
+        if #available(iOS 15, *) {
+            let appearance = UINavigationBarAppearance()
+            if sender.isOn {
+                appearance.configureWithTransparentBackground()
+            } else {
+                appearance.configureWithOpaqueBackground()
+            }
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        } else {
+            navigationController?.navigationBar.isTranslucent = sender.isOn
+        }
     }
 }
 
