448fc5cbb4cf6cfc1f79c623fee4255aa3d7650f
Shin Yamamoto <shin@scenee.com>
2023-04-08 10:30:11 +0900

    Stop changing UIScrollView.bounces when locking or unlocking a scroll view (#584)
    UIScrollView would unexpectedly change its scroll offset after updating the bounces property when dealing with small scrollable content. This fixes issue #524, "Scrolling jumps when tableView content is small".

--- Sources/Extensions.swift
+++ Sources/Extensions.swift
@@ -157,7 +157,7 @@ extension UIGestureRecognizer.State: CustomDebugStringConvertible {
 
 extension UIScrollView {
     var isLocked: Bool {
-        return !showsVerticalScrollIndicator && !bounces &&  isDirectionalLockEnabled
+        return !showsVerticalScrollIndicator && isDirectionalLockEnabled
     }
     var fp_contentInset: UIEdgeInsets {
         if #available(iOS 11.0, *) {
