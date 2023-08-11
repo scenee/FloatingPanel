scenee/FloatingPanel

commit 7511ce577d184ed4e49b78e77cf7811a0b1dd86e

Fix scrollview content staying non-interactive after slowly swiping down (#597)
This was noticed when updating contained SwiftUI views rapidly.

This change is what fixed a certain bug the scroll content can be locked at a negative offset. It was most obvious when the whitespace was large, hence the offset was something around -100.0. At the same time, the contents (like buttons) were non-interactive.

Co-authored-by: Sören Gade <soeren.gade@lichtblick.de>

Sources/Core.swift
@@ -1079,7 +1079,11 @@ class Core: NSObject, UIGestureRecognizerDelegate {
         // Must use setContentOffset(_:animated) to force-stop deceleration
         guard let scrollView = scrollView else { return }
         var offset = scrollView.contentOffset
-        setValue(contentOffset, to: &offset)
+        if contentOffset.y >= 0 {
+            setValue(contentOffset, to: &offset)
+        } else {
+            offset = CGPoint(x: 0, y: 0)
+        }
         scrollView.setContentOffset(offset, animated: false)
     }
 

commit 85ed3a6ce352616f0be7accbadafd5c474bd3b87

Fix scroll tracking issues of the scroll view with a positive scroll inset
These issues arose in 'Show Navigation Controller' sample of Samples app.

1. The scrollView's contentOffset always becomes (0, 0) instead of (0, -44),
   which is normal if there is a UINavigationBar.

2. The scrollView's contentOffset sometimes becomes (0, 0) after moving
   a panel quickly like picking.

Case 1 is caused by 7511ce5 commit.
Case 2 is caused by a workaround added at 81fd85e commit.

I tested this library from iOS 11 to iOS 16, and then I confirmed this
workaround doesn't need anymore.

Related to #602, #603.

Sources/Core.swift
@@ -719,10 +719,12 @@ class Core: NSObject, UIGestureRecognizerDelegate {
         // Determine whether the panel's dragging should be projected onto the scroll content scrolling
         stopScrollDeceleration = 0 > layoutAdapter.offsetFromMostExpandedAnchor
         if stopScrollDeceleration {
+            os_log(msg, log: devLog, type: .debug, "panningEnd -- will stop scrolling at initialScrollOffset = \(initialScrollOffset)")
             DispatchQueue.main.async { [weak self] in
                 guard let self = self else { return }
-                
+
                 self.stopScrolling(at: self.initialScrollOffset)
+                os_log(msg, log: devLog, type: .debug, "panningEnd -- did stop scrolling at initialScrollOffset = \(self.initialScrollOffset)")
             }
         }
 
@@ -765,19 +767,7 @@ class Core: NSObject, UIGestureRecognizerDelegate {
             vc.delegate?.floatingPanelDidEndDragging?(vc, willAttract: true)
         }
 
-        // Workaround: Disable a tracking scroll to prevent bouncing a scroll content in a panel animating
-        let isScrollEnabled = scrollView?.isScrollEnabled
-        if let scrollView = scrollView, targetPosition != layoutAdapter.mostExpandedState {
-            scrollView.isScrollEnabled = false
-        }
-
         startAttraction(to: targetPosition, with: velocity)
-
-        // Workaround: Reset `self.scrollView.isScrollEnabled`
-        if let scrollView = scrollView, targetPosition != layoutAdapter.mostExpandedState,
-            let isScrollEnabled = isScrollEnabled {
-            scrollView.isScrollEnabled = isScrollEnabled
-        }
     }
 
     // MARK: - Behavior
@@ -813,7 +803,7 @@ class Core: NSObject, UIGestureRecognizerDelegate {
                 initialScrollOffset = scrollView.contentOffset
             } else {
                 let pinningOffset = contentOffsetForPinning(of: scrollView)
-                
+
                 // `initialScrollOffset` must be reset to the pinning offset because the value of `scrollView.contentOffset`,
                 // for instance, is a value in [-30, 0) on a bottom positioned panel with `allowScrollPanGesture(of:condition:)`.
                 // If it's not reset, the following logic to shift the surface frame will not work and then the scroll
@@ -1090,11 +1080,7 @@ class Core: NSObject, UIGestureRecognizerDelegate {
         // Must use setContentOffset(_:animated) to force-stop deceleration
         guard let scrollView = scrollView else { return }
         var offset = scrollView.contentOffset
-        if contentOffset.y >= 0 {
-            setValue(contentOffset, to: &offset)
-        } else {
-            offset = CGPoint(x: 0, y: 0)
-        }
+        setValue(contentOffset, to: &offset)
         scrollView.setContentOffset(offset, animated: false)
     }
 