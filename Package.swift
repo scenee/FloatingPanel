c9ae0d7e9273fe036c622183f6e69e8e8f56f511
Shin Yamamoto <shin@scenee.com>
2025-04-27 21:00:06 +0900

    Update the minimum deployment target to iOS 13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FloatingPanel",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
