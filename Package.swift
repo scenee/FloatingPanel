3ff3fc565a50a4c53837bd217bbfda1c1b277959
Shin Yamamoto <shin@scenee.com>
2025-04-27 21:00:06 +0900

    Update the minimum deployment target to iOS 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "FloatingPanel",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
