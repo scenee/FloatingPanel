54f1cc17eec5eb5a3348e43df719a399b0444e76
Shin Yamamoto <shin@scenee.com>
2025-04-30 09:32:59 +0900

    Take care of file paths including non-ASCII chars in SwiftFormatBuildToolPlugin

--- BuildTools/Plugins/swift-format-plugin.swift
+++ BuildTools/Plugins/swift-format-plugin.swift
@@ -4,6 +4,7 @@ import PackagePlugin
 @main
 struct SwiftFormatBuildToolPlugin: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        // Currently the build tool plugin is not supported.
         return []
     }
 }
@@ -11,6 +12,7 @@ struct SwiftFormatBuildToolPlugin: BuildToolPlugin {
 #if canImport(XcodeProjectPlugin)
 import XcodeProjectPlugin
 
+/// Formats Swift source files using the `swift format` command and a root configuration file during Xcode builds.
 extension SwiftFormatBuildToolPlugin: XcodeBuildToolPlugin {
     // Entry point for creating build commands for targets in Xcode projects.
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
@@ -22,16 +24,17 @@ extension SwiftFormatBuildToolPlugin: XcodeBuildToolPlugin {
         let xcodeProjectDirectoryURL = URL(fileURLWithPath: context.xcodeProject.directory.string)
         #endif
         // Find the code generator tool to run (replace this with the actual one).
-        print("SwiftFormatBuildToolPlugin -> \(xcodeProjectDirectoryURL.path())")
-        let configFile = xcodeProjectDirectoryURL.appending(path: ".swift-format")
-        // Currently check only 'SwiftUI' source code.
-        let sourceFiles = xcodeProjectDirectoryURL.appending(path: "Sources/SwiftUI")
-        // let sourceFiles = xcodeProjectDirectoryURL.appending(path: "Sources")
-        // let testFiles = xcodeProjectDirectoryURL.appending(path: "Tests")
-        let buildToolsFiles = xcodeProjectDirectoryURL.appending(path: "BuildTools")
-        let examplesFiles = [
-            xcodeProjectDirectoryURL.appending(path: "Examples/SamplesSwiftUI").path()
-        ]
+        print("SwiftFormatBuildToolPlugin -> \(xcodeProjectDirectoryURL.filePath)")
+        let configFile = xcodeProjectDirectoryURL.appending(path: ".swift-format").filePath
+        let targetFiles = [
+            // Currently check only 'SwiftUI' source code in 'Sources' dir.
+            xcodeProjectDirectoryURL.appending(path: "Sources/SwiftUI"),
+            // xcodeProjectDirectoryURL.appending(path: "Sources"),
+            // xcodeProjectDirectoryURL.appending(path: "Tests"),
+            xcodeProjectDirectoryURL.appending(path: "BuildTools"),
+            // Currently check only 'SamplesSwiftUI' source code in 'Examples' dir.
+            xcodeProjectDirectoryURL.appending(path: "Examples/SamplesSwiftUI"),
+        ].map { $0.filePath }
         return [
             .buildCommand(
                 displayName: "Run swift format(xcode)",
@@ -40,16 +43,18 @@ extension SwiftFormatBuildToolPlugin: XcodeBuildToolPlugin {
                     "format",
                     "lint",
                     "--configuration",
-                    configFile.path(),
+                    configFile,
                     "-r",
-                    sourceFiles.path(),
-                    //testFiles.path(),
-                    buildToolsFiles.path(),
-                ] + examplesFiles,
+                ] + targetFiles,
                 inputFiles: [],
                 outputFiles: []
             )
         ]
     }
+}
+
+extension URL {
+    /// Returns a non–percent-encoded path for use with components containing non-ASCII characters.
+    var filePath: String { path(percentEncoded: false) }
 }
 #endif
